62089a52208c45ceca918831b316c863cd1a4809
David <david_gu@yahoo.com>
2023-06-19 04:45:49 -0700

    get spots of current user

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -51,4 +51,48 @@ router.get(
         })
         res.json(spotArr)
     })
+
+    router.get('/current',
+        async (req, res) => {
+            const {user} = req;
+            console.log(typeof user.id)
+            const payload = await Spot.findAll({
+                where:{ ownerId : user.id},
+                include:
+            [
+                {
+                    model: Review
+                },
+                {
+                    model:SpotImage
+                }
+            ]
+            })
+            const Spots = [];
+            payload.forEach(spot => {
+                Spots.push(spot.toJSON())
+            });
+            Spots.forEach(spot => {
+                spot.SpotImages.forEach(image => {
+                    if(image.preview){
+                        spot.previewImage = image.url
+                    }
+                });
+                let stars = 0;
+                let counter =0;
+                spot.Reviews.forEach(review=> {
+                    stars+=review.stars
+                    counter+=1
+                })
+                spot.avgRating = stars/counter
+                counter=0
+                stars= 0;
+
+                delete spot.SpotImages
+            });
+            Spots.forEach(ele => {
+                delete ele.Reviews
+            })
+            res.json(Spots)
+        })
 module.exports = router;
