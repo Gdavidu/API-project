6ca90fa629096fa2bf3b51baee1892b7ebba72a3
David <david_gu@yahoo.com>
2023-07-12 09:56:24 -0700

    Create a Spot

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -49,13 +49,13 @@ router.get(
         spotArr.forEach(ele => {
             delete ele.Reviews
         })
-        res.json(spotArr)
+        res.json({"Spots": spotArr})
     })
 
 router.get('/current',
     async (req, res) => {
         const { user } = req;
-        console.log(typeof user.id)
+        // console.log(typeof user.id)
         const payload = await Spot.findAll({
             where: { ownerId: user.id },
             include:
@@ -93,7 +93,7 @@ router.get('/current',
         Spots.forEach(ele => {
             delete ele.Reviews
         })
-        res.json(Spots)
+        res.json({"Spots": Spots})
     })
 
 router.get('/:id',
@@ -116,10 +116,10 @@ router.get('/:id',
                 ]
         })
 
-        if (!payload){
+        if (!payload) {
             const err = new Error("Spot couldn't be found")
             err.status = 404
-           return next(err)
+            return next(err)
         }
         payload.dataValues.Owner = payload.dataValues.User
         delete payload.dataValues.User
@@ -138,4 +138,48 @@ router.get('/:id',
         delete payload.dataValues.Reviews
         res.json(payload)
     })
+const validateSpot = [
+    check('address')
+        .exists({ checkFalsy: true })
+        .withMessage("Street address is required"),
+    check('city')
+        .exists({ checkFalsy: true })
+        .withMessage("City is required"),
+    check('state')
+        .exists({ checkFalsy: true })
+        .withMessage("State is required"),
+    check('country')
+        .exists({ checkFalsy: true })
+        .withMessage("Country is required"),
+    check('lat')
+        .exists({ checkFalsy: true })
+        .isNumeric()
+        .withMessage("Latitude is not valid"),
+    check('lng')
+        .exists({ checkFalsy: true })
+        .isNumeric()
+        .withMessage("Longitude is not valid"),
+    check('name')
+        .exists({ checkFalsy: true })
+        .isLength({ max: 49 })
+        .withMessage("Name must be less than 50 characters"),
+    check('description')
+        .exists({ checkFalsy: true })
+        .withMessage("Description is required"),
+    check('price')
+        .exists({ checkFalsy: true })
+        .withMessage("Price per day is required"),
+    handleValidationErrors
+];
+
+router.post('/', validateSpot,
+    async (req, res, next) => {
+        const {address, city,state,country, lat , lng, name, description,price} = req.body
+        const {user} = req
+        // console.log(user.id)
+        const spot = await Spot.create({ownerId:user.id, address, city,state,country, lat , lng, name, description,price})
+        // const
+        return res.json({spot})
+    })
+
 module.exports = router;
