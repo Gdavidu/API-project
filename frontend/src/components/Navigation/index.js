ea587409d7d05b147911a9228576d0d7953d4613
David <david_gu@yahoo.com>
2023-07-31 13:50:41 -0700

    Started Navigation css

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -25,12 +25,14 @@ function Navigation({ isLoaded }){
   }
 
   return (
+
     <ul>
       <li>
         <NavLink exact to="/">Home</NavLink>
       </li>
       {isLoaded && sessionLinks}
     </ul>
+   
   );
 }
 
