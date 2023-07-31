1b0d4dc7eaa8c9fb97de54b18b96355f7bad9a5c
David <david_gu@yahoo.com>
2023-07-31 15:33:35 -0700

    Auth me bonus nest login/signup in dropdown

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormModal";
-import SignupFormPage from "./components/SignupFormModal";
+import { Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 
