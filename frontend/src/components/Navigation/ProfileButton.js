33498dc9958a8e10d90ac479683858da44c8aae4
David <david_gu@yahoo.com>
2023-07-31 11:32:53 -0700

    Navigation and Profile button

---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,8 @@
+const ProfileButton = () => {
+    return (
+      <div style={{ color: "darkcyan", fontSize: "30px" }}>
+       <i class="fa-solid fa-user"></i>
+      </div>
+    );
+  };
+ export default ProfileButton
