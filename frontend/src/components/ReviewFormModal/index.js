2f753f0843b5bdc5a245c9eb7036f4cead66fc76
David <david_gu@yahoo.com>
2023-08-06 17:51:59 -0700

    reset and now only review modal popup functional

--- frontend/src/components/ReviewFormModal/index.js
+++ frontend/src/components/ReviewFormModal/index.js
@@ -1,21 +1,43 @@
 import './ReviewForm.css'
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { postSpotReview } from '../../store/reviews';
+import { useModal } from '../../context/Modal';
+import { useParams } from 'react-router-dom'
+import { fetchSpotDetail } from '../../store/spots';
 function ReviewFormModal() {
+    const { closeModal } = useModal();
+    const spotId = useSelector(state => state.spots.singleSpot.id)
+    console.log('id from review form', spotId)
+    const dispatch = useDispatch()
     const [comment, setComment] = useState("")
     const [errors, setErrors] = useState({});
     const [rating, setRating] = useState(0);
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         setErrors({});
 
-        const newReview ={review: comment, stars: rating}
+        // const newReview ={review: comment, stars: rating}
+        // const sentReview = await dispatch(postSpotReview(newReview, spotId))
+        // console.log('errors', sentReview.errors)
+        // if (sentReview.errors) {
+
+        //     setErrors(sentReview.errors)
+        // }
+        // else{
+        //     await dispatch(fetchSpotDetail(spotId)).then(closeModal)
+
+        // }
     }
     return (
         <>
             <div>
                 <h1>How was your stay?</h1>
+                {errors.message && <div className='errors'>{errors.message}</div>}
+                {errors.stars && <div className='errors'>{errors.stars}</div>}
+                {errors.review && <div className='errors'>{errors.review}</div>}
                 <input
                 type='text'
                 placeholder="Leave your review here..."
