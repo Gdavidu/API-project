c1c8af1a48d793fbaef23c184bedf6f64dac282c
David <david_gu@yahoo.com>
2023-08-07 08:45:56 -0700

    seeder glitch

--- backend/db/seeders/20230618235256-SpotImages-Seeder.js
+++ backend/db/seeders/20230618235256-SpotImages-Seeder.js
@@ -11,42 +11,42 @@ module.exports = {
     await queryInterface.bulkInsert(options, [
       {
         spotId: 1,
-        url: <img src="https://a0.muscache.com/im/pictures/miso/Hosting-586898398649103226/original/9c689aaa-2364-4f1f-ae9c-d56bda466912.jpeg?im_w=1200"></img>,
+        url: "https://a0.muscache.com/im/pictures/miso/Hosting-586898398649103226/original/9c689aaa-2364-4f1f-ae9c-d56bda466912.jpeg?im_w=1200",
         preview: true
       },
       {
         spotId: 1,
-        url: <img src="https://a0.muscache.com/im/pictures/miso/Hosting-586898398649103226/original/9e6c2409-c192-4e1f-9c05-be4eb486051c.jpeg?im_w=720"></img>,
+        url: "https://a0.muscache.com/im/pictures/miso/Hosting-586898398649103226/original/9e6c2409-c192-4e1f-9c05-be4eb486051c.jpeg?im_w=720",
         preview: true
       },
       {
         spotId: 1,
-        url: <img src="https://a0.muscache.com/im/pictures/miso/Hosting-586898398649103226/original/c6c5f145-42e4-48ba-aaaf-c1a15cd68ae4.jpeg?im_w=720"></img>,
+        url: "https://a0.muscache.com/im/pictures/miso/Hosting-586898398649103226/original/c6c5f145-42e4-48ba-aaaf-c1a15cd68ae4.jpeg?im_w=720",
         preview: true
       },
       {
         spotId: 1,
-        url: <img src="https://a0.muscache.com/im/pictures/miso/Hosting-586898398649103226/original/06dd2926-c7d9-4846-8534-37fb0b5a8cd3.jpeg?im_w=1200"></img>,
+        url: "https://a0.muscache.com/im/pictures/miso/Hosting-586898398649103226/original/06dd2926-c7d9-4846-8534-37fb0b5a8cd3.jpeg?im_w=1200",
         preview: true
       },
       {
         spotId: 1,
-        url: <img src="https://a0.muscache.com/im/pictures/miso/Hosting-586898398649103226/original/75596971-c074-4259-9611-ba5663433f74.jpeg?im_w=720"></img>,
+        url: "https://a0.muscache.com/im/pictures/miso/Hosting-586898398649103226/original/75596971-c074-4259-9611-ba5663433f74.jpeg?im_w=720",
         preview: true
       },
       {
         spotId: 3,
-        url: <img src="https://a0.muscache.com/im/pictures/miso/Hosting-586898398649103226/original/0a8643e3-a2b3-41c8-bca9-9b7e8a00c9f5.jpeg?im_w=1200"></img>,
+        url: "https://a0.muscache.com/im/pictures/miso/Hosting-586898398649103226/original/0a8643e3-a2b3-41c8-bca9-9b7e8a00c9f5.jpeg?im_w=1200",
         preview: true
       },
       {
         spotId: 2,
-        url: <img src="https://nationaltoday.com/wp-content/uploads/2022/08/27-McDonalds-Day-1200x834.jpg"></img>,
+        url: "https://nationaltoday.com/wp-content/uploads/2022/08/27-McDonalds-Day-1200x834.jpg",
         preview: true
       },
       {
         spotId: 4,
-        url: <img src="https://a0.muscache.com/im/pictures/prohost-api/Hosting-879012498547289208/original/e24abccc-fbd9-4b42-a8b6-7b57598c13fc.jpeg?im_w=720"></img>,
+        url: "https://a0.muscache.com/im/pictures/prohost-api/Hosting-879012498547289208/original/e24abccc-fbd9-4b42-a8b6-7b57598c13fc.jpeg?im_w=720",
         preview: true
       },
       {
